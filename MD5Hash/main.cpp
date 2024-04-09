6cd94eff7d8a91b13a2d1515d4683ca3f2876b1d
Matthew <nguyenhobbes2002@gmail.com>
2024-04-09 01:48:29 -0400

    kinda finished

--- MD5Hash/main.cpp
+++ MD5Hash/main.cpp
@@ -36,6 +36,9 @@ string bwNOT(string str);
 void leftRotateBin(string& s, int amount);
 
 string functionF(string b, string c, string d);
+string functionG(string b, string c, string d);
+string functionH(string b, string c, string d);
+string functionI(string b, string c, string d);
 
 int main() {
 	string text = "They are deterministic";
@@ -307,30 +310,153 @@ void leftRotateBin(string& binaryStr, int shiftAmount) {
 
 void hash512Block(string s) {
 	string temp;
+
+	//original;
+	string oa = "01234567";
+	string ob = "89abcdef";
+	string oc = "fedcba98";
+	string od = "76543210";
+
+	//dynamic, will change later
 	string a = "01234567";
 	string b = "89abcdef";
 	string c = "fedcba98";
 	string d = "76543210";
 
+
+	string F;
+	long long int x;
+	long long int y;
+	long long int Fa;
+
+	string ans;
+
+
+	int r[4][16] = { { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 },
+		{ 1,6,11,0,5,10,15,4,9,14,3,8,13,2,7,12 },
+		{ 5,8,11,14,1,4,7,10,13,0,3,6,9,12,15,2 },
+		{ 0,7,14,5,12,3,10,1,8,15,6,13,4,11,2,9 } };
+	string m[16];
+	for (int i = 0; i < 16; i++)
+	{
+		m[i] = s.substr(8 * i, 8);
+	}
 	string k[64];
 	for (int i = 0; i < 64; i++)
 	{
 		k[i] = decToHex(floor(mod * abs(sin(i + 1))));
 		//cout << "K[" << i + 1 << "] = " << k[i] << endl;
 	}
+	int shift[4][16];
+	for (int i = 1; i <= 4; i++)
+	{
+		for (int j = 1; j <= 16; j++)
+		{
+			if (i == 1)
+			{
+				if (j == 1 || j == 5 || j == 9 || j == 13)
+				{
+					shift[i - 1][j - 1] = 7;
+				}
+				else if (j == 2 || j == 6 || j == 10 || j == 14)
+				{
+					shift[i - 1][j - 1] = 12;
+				}
+				else if (j == 3 || j == 7 || j == 11 || j == 15)
+				{
+					shift[i - 1][j - 1] = 17;
+				}
+				else
+				{
+					shift[i - 1][j - 1] = 22;
+				}
+			}
+			else if (i == 2)
+			{
+				if (j == 1 || j == 5 || j == 9 || j == 13)
+				{
+					shift[i - 1][j - 1] = 5;
+				}
+				else if (j == 2 || j == 6 || j == 10 || j == 14)
+				{
+					shift[i - 1][j - 1] = 9;
+				}
+				else if (j == 3 || j == 7 || j == 11 || j == 15)
+				{
+					shift[i - 1][j - 1] = 14;
+				}
+				else
+				{
+					shift[i - 1][j - 1] = 20;
+				}
+			}
+			else if (i == 3)
+			{
+				if (j == 1 || j == 5 || j == 9 || j == 13)
+				{
+					shift[i - 1][j - 1] = 4;
+				}
+				else if (j == 2 || j == 6 || j == 10 || j == 14)
+				{
+					shift[i - 1][j - 1] = 11;
+				}
+				else if (j == 3 || j == 7 || j == 11 || j == 15)
+				{
+					shift[i - 1][j - 1] = 16;
+				}
+				else
+				{
+					shift[i - 1][j - 1] = 13;
+				}
+
+			}
+			else
+			{
+				if (j == 1 || j == 5 || j == 9 || j == 13)
+				{
+					shift[i - 1][j - 1] = 6;
+				}
+				else if (j == 2 || j == 6 || j == 10 || j == 14)
+				{
+					shift[i - 1][j - 1] = 10;
+				}
+				else if (j == 3 || j == 7 || j == 11 || j == 15)
+				{
+					shift[i - 1][j - 1] = 15;
+				}
+				else
+				{
+					shift[i - 1][j - 1] = 21;
+				}
+			}
+		}
+	}
 
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 16 * i; j < 16 * (i + 1); j++) {
-			temp = s.substr(8 * j, 8);
-			cout << "---------------ROUND " << j + 1 << ": " << temp << endl;
+			temp = m[r[i][j % 16]];
+			cout << "---------------ROUND " << j + 1 << ": " << m[r[i][j % 16]]  << endl;
+			cout << a << endl << b << endl << c << endl << d << endl << endl;
 			//compute F and add A to F
-			string F = functionF(b, c, d);
+			switch (i)
+			{
+			default:
+				break;
+			case 0: 
+				F = functionF(b, c, d);
+			case 1:
+				F = functionG(b, c, d);
+			case 2:
+				F = functionH(b, c, d);
+			case 3:
+				F = functionI(b, c, d);
+			}
 			cout << F << endl;
-			long long int x = hexToDec(a);
-			long long int y = hexToDec(F);
-			long long int Fa = (x + y) % mod;
-			string ans = decToHex(Fa);
+			x = hexToDec(a);
+			y = hexToDec(F);
+			Fa = (x + y) % mod;
+			ans = decToHex(Fa);
 			cout << ans << endl;
 
 			//Add message Mi
@@ -350,7 +476,7 @@ void hash512Block(string s) {
 
 			//Rotate left
 			temp = hexToBin(ans);
-			leftRotateBin(temp, 7);
+			leftRotateBin(temp, shift[i][j]);
 			ans = binToHex(temp);
 			cout << ans << endl;
 
@@ -368,9 +494,31 @@ void hash512Block(string s) {
 			b = ans;
 
 
-			cout << a << endl << b << endl << c << endl << d << endl;
+			cout << endl << "4 vectors now are:\n" << a << endl << b << endl << c << endl << d << endl;
 		}
 	}
+	string finA, finB, finC, finD;
+	x = hexToDec(a);
+	y = hexToDec(oa);
+	Fa = (x + y) % mod;
+	finA = decToHex(Fa);
+
+	x = hexToDec(b);
+	y = hexToDec(ob);
+	Fa = (x + y) % mod;
+	finB = decToHex(Fa);
+
+	x = hexToDec(c);
+	y = hexToDec(oc);
+	Fa = (x + y) % mod;
+	finC = decToHex(Fa);
+
+	x = hexToDec(d);
+	y = hexToDec(od);
+	Fa = (x + y) % mod;
+	finD = decToHex(Fa);
+
+	cout << finA << endl << finB << endl << finC << endl << finD << endl;
 
 	
 
@@ -378,6 +526,18 @@ void hash512Block(string s) {
 
 string functionF(string b, string c, string d) {
 	return bwOR(bwAND(b, c), bwAND(bwNOT(b), d));
+}
+
+string functionG(string b, string c, string d) {
+	return bwOR(bwAND(b, d), bwAND(bwNOT(d), c));
+}
+
+string functionH(string b, string c, string d) {
+	return bwXOR(bwXOR(b, c), d);
+}
+
+string functionI(string b, string c, string d) {
+	return bwXOR(c, bwOR(bwNOT(d), b));
 }
 
 
